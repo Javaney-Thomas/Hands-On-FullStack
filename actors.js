319378d5fcf24c8e3bb9f0dda35690da71185774
Javaney <javaneyt@icloud.com>
2023-03-02 12:58:02 -0600

    Morechanges, actors is the data to work with

---
+++ actors.js
@@ -0,0 +1,3 @@
+//console.error(node actors.js [read | create | update | destroy])
+console.log(typeof process.argv[2])//All the arguments in the method are strings starting at 2
+var actornum = process.argv[2]
